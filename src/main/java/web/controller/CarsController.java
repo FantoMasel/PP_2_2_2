8c16ee36969614f7318807d33d7f848488026b74
Alexey Maslov <fantomasel@gmail.com>
2022-10-17 22:31:42 +0300

    2.2.2(2)

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarsServiceImpl;
 
@@ -20,15 +18,9 @@ public class CarsController {
 
 
     @GetMapping("/cars")
-    public String getAllCars(@RequestParam(defaultValue =  "count") String count, Model model){
-        if(count.equals("count")){
-            model.addAttribute("cars", carsService.getCars(5));
-            return "cars";
-        }else{
-            int result = Integer.parseInt(count);
-            model.addAttribute("cars", carsService.getCars(result));
-            return "cars";
-        }
+    public String getAllCars(@RequestParam(defaultValue =  "5") int count, Model model){
+            model.addAttribute("cars", carsService.getCars(count));
+        return "cars";
     }
 
 
